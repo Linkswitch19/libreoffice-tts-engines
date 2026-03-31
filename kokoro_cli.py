88e903c0895d5e162d177e59028721741770f810
andre.peitiocampo <andre.peitiocampo>
2026-03-31 05:42:33 +0200

    Engine Client kokoro code

---
+++ kokoro_cli.py
@@ -0,0 +1,66 @@
+import argparse
+import soundfile as sf
+#without PyTorch
+from kokoro_onnx import Kokoro
+import sys
+import os
+
+
+
+
+
+
+def main():
+    args_tts =parser_TTSKokoro()
+    model_path,voices_path=extration_path()
+    samples, saple_rate=kokoro_engine_generation(args_tts,model_path,voices_path)
+    #save file
+    sf.write(args_tts.output,samples,saple_rate)
+    print(f"Saved in {args_tts.output}")
+
+
+
+def kokoro_engine_generation(args_tts,model_path,voices_path):
+     #start Kokoro
+    kokoro_tts= Kokoro(model_path,voices_path)
+    
+    #generate audio
+    print(f"Generate audio for: {args_tts.text[:30]}...")
+    samples, saple_rate = kokoro_tts.create(
+        args_tts.text,
+        voice=args_tts.voice,
+        speed=1.0,
+        lang=args_tts.lang
+    )
+    return samples, saple_rate
+    
+
+def extration_path():
+    #check if im using the exe and choose the path 
+    if getattr(sys, 'frozen',False):
+        #extract the file here
+        base_path= sys._MEIPASS
+    else:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        
+    model_path = os.path.join(base_path, "kokoro-v1.0.int8.onnx")
+    voices_path = os.path.join(base_path, "voices-v1.0.bin")
+    if not os.path.exists(model_path) or not os.path.exists(voices_path):
+        print(f"Error: file of model or voice not found in {base_path}")
+        sys.exit(1)
+    return model_path,voices_path
+    
+         
+def parser_TTSKokoro():
+    """
+    Parser for to read args of the voice 
+    """
+    parser = argparse.ArgumentParser(description='Generatore TTS Kokoro ONNX')
+    parser.add_argument('--text',type=str,required=True, help="Text to read")
+    parser.add_argument('--output',type=str, required=True, help='Path to save .wav')
+    parser.add_argument('--lang', type=str, default= 'en-us', help='Language (ex. en-us,it)')
+    parser.add_argument('--voice', type=str ,default='af_heart', help = 'Select voice')
+    return parser.parse_args()
+    
+if __name__ == "__main__":
+    main()
